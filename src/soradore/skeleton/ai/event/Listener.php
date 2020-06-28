a45e91035cd1a77fa270c0397f9140f3aa9d5d2c
soradore <soradore@svk.jp>
2020-06-28 13:03:10 +0900

    Update Listener.php

--- src/soradore/skeleton/ai/event/Listener.php
+++ src/soradore/skeleton/ai/event/Listener.php
@@ -17,12 +17,12 @@ class Listener implements EventListener {
         $this->plugin = $plugin;
     }
 
-    public function onJoin(PlayerJoinEvent $ev){
+    /*public function onJoin(PlayerJoinEvent $ev){
         $player = $ev->getPlayer();
         $nbt = Entity::createBaseNBT($player);
         $skeleton = Entity::createEntity("Skeleton", $player->getLevel(), $nbt);
         $skeleton->spawnToAll();
-    }
+    }*/
 
     public function onDamage(EntityDamageEvent $ev)
     {
