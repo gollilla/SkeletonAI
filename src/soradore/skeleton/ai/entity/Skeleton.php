9a4f03e11507e0d48191871c3f8734326eff3923
gollilla <soradore2@gmail.com>
2021-10-31 13:17:41 +0900

    api 4.0.0

--- src/soradore/skeleton/ai/entity/Skeleton.php
+++ src/soradore/skeleton/ai/entity/Skeleton.php
@@ -2,34 +2,52 @@
 
 namespace soradore\skeleton\ai\entity;
 
-use pocketmine\level\Level;
-use pocketmine\entity\Monster;
+use pocketmine\world\World;
+use pocketmine\entity\Living;
 use pocketmine\entity\Entity;
-use pocketmine\Player;
+use pocketmine\entity\EntitySizeInfo;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\player\Player;
 use pocketmine\math\Vector3;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 
-class Skeleton extends Monster {
-
-    public const NETWORK_ID = self::SKELETON;
+class Skeleton extends Living {
 
     private $target = null;
+    private $isNeutral = true;
+
     private $speed = 0.28;
     private $coolTime = 0;
 
-    public $width = 0.6;
-    public $height = 1.9;
+    public static function getNetworkTypeId() : string{ return EntityIds::SKELETON; }
+
+    protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(1.8, 0.6); //TODO: eye height ??
+	}
     
     public function getName() : string{
-	return "Skeleton";
+	    return "Skeleton";
+    }
+
+    public function findClosestPlayer(int $distance) : ?Player {
+        $result = null;
+        foreach ($this->getWorld()->getPlayers() as $player) {
+            //[$playerとこのエンティティの距離 < 前の$playerの距離]なら、$resultに$playerを代入
+            if ($player->location->distance($this->location) < $distance) {
+                $result = $player;//結果に代入
+                $distance = $player->location->distance($this->location);//距離を更新
+            }
+        }
+
+        return $result;
     }
 
     public function entityBaseTick(int $tickDiff = 1): bool
     {
-        $level = $this->getLevel();
-        $time = $level->getTimeOfDay();
-        if(0 <= $time && $time < Level::TIME_NIGHT){
+        $world = $this->getWorld();
+        $time = $world->getTimeOfDay();
+        if(0 <= $time && $time < World::TIME_NIGHT){
             $this->kill();
         }
         $hasUpdate = parent::entityBaseTick($tickDiff);
@@ -41,28 +59,46 @@ class Skeleton extends Monster {
         else
             $this->attackTime = 0;
         
-        if($this->getTarget() == NULL)
-            return $hasUpdate;
+        if($this->getTarget() == null) {
+            if ($this->isNeutral) return $hasUpdate;//中立の状態なら処理を終了
+
+            $preTarget = $this->findClosestPlayer(10);
+            if ($preTarget === null) {
+                $this->isNeutral = true;//中立状態に設定
+                return $hasUpdate;//プレイヤーが近くにいなければ処理を終了
+            } else {
+                $this->isNeutral = false;//中立状態を解除
+                $this->target = $preTarget;
+            }
+        }
 
         $target = $this->getTarget();
         if(!($target instanceof Player))
             return $hasUpdate;
         
         $speed = $this->getSpeed();
-        $this->lookAt($target);
+        $this->lookAt($target->location);
 
-        if($this->distance($target) <= 1){
+        if($this->location->distance($target->location) <= 1){
             if($this->coolTime < 0){
                 $ev = new EntityDamageByEntityEvent($this, $target, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 3);;
                 $target->attack($ev);
                 $this->coolTime = 23;
             }
             return $hasUpdate;
+        } else if ($this->location->distance($target->location) >= 5) {//5ブロックより遠ければ
+            $preTarget = $this->findClosestPlayer(10);//10ブロック以内の一番近いプレイヤーを取得
+            if ($preTarget === null) {//プレイヤーが近くにいなければ
+                $this->target = null;//ターゲットを空にして、処理をやめる。
+                return $hasUpdate;
+            } else {//プレイヤーが存在すれば
+                $this->target = $preTarget;//ターゲットを設定
+            }
         }
             
 
-        $moveX = sin(-deg2rad($this->yaw)) * $speed;
-        $moveZ = cos(-deg2rad($this->yaw)) * $speed;
+        $moveX = sin(-deg2rad($this->location->yaw)) * $speed;
+        $moveZ = cos(-deg2rad($this->location->yaw)) * $speed;
         $this->checkFront();
         $this->motion->x = $moveX;
         $this->motion->z = $moveZ;
@@ -91,12 +127,12 @@ class Skeleton extends Monster {
     public function checkFront(): void
     {
         $dv = $this->getDirectionVector()->multiply(1);
-        $checkPos = $this->add($dv->x, 0, $dv->z)->floor();
-        if($this->level->getBlockAt($checkPos->x, $this->y+1, $checkPos->z)->isSolid())
+        $checkPos = $this->location->add($dv->x, 0, $dv->z)->floor();
+        if($this->getWorld()->getBlockAt($checkPos->x, $this->location->y+1, $checkPos->z)->isSolid())
         {
             return;
         }
-        if($this->level->getBlockAt($checkPos->x, $this->y, $checkPos->z)->isSolid())
+        if($this->getWorld()->getBlockAt($checkPos->x, $this->location->y, $checkPos->z)->isSolid())
         {
             $this->jump();
         }
@@ -105,6 +141,7 @@ class Skeleton extends Monster {
 
     public function setTarget(Player $player)
     {
+        $this->isNeutral = false;
         $this->target = $player;
     }
 
@@ -126,7 +163,6 @@ class Skeleton extends Monster {
     }
 
     public function getXpDropAmount() : int{
-	return 0;
+	    return 0;
     }
-
 }
